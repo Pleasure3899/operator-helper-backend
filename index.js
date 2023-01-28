9486850159a148dab4e8df84515d54ef727785e2
Pleasure3899 <rudyk.vitaliy99@gmail.com>
2023-01-28 19:08:09 +0200

    Added get and post methods for objects table

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
 import express from 'express'
+import mysql from 'mysql'
 
 const app = express()
+app.use(express.json());
 
-app.listen(8800, ()=> {
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "operator_helper",
+});
+
+//test
+app.get("/", (request, response) => {
+    response.json("hello");
+});
+
+//get pbjects
+app.get("/objects", (request, response) => {
+    const query = "SELECT * FROM objects"
+    db.query(query, (error, data) => {
+        if (error) {
+            console.log(error);
+            return response.json(error);
+        }
+        return response.json(data);
+    })
+});
+
+//add object
+app.post("/objects", (request, response) => {
+    const q = "INSERT INTO objects(`id`, `latitude`, `longitude`) VALUES (?)";
+  
+    const values = [
+      request.body.id,
+      request.body.latitude,
+      request.body.longitude,
+    ];
+  
+    db.query(q, [values], (error, data) => {
+      if (error) return response.send(error);
+      return response.json(data);
+    });
+  });
+
+app.listen(8800, () => {
     console.log("Connected to backend")
 })
