19e5d97dfffd1038839118ac6f1d4937aa895c76
Pleasure3899 <rudyk.vitaliy99@gmail.com>
2023-02-19 19:22:48 +0200

    Added requests for incidents

--- index.js
+++ index.js
@@ -335,9 +335,21 @@ app.get("/incidents", (request, response) => {
   })
 });
 
-//get opened incidents
+//get incidents with join
+app.get("/incidents-join", (request, response) => {
+  const query = "SELECT incidents.id AS incidentid, incidents.is_checked AS incidentchecked, incidents.iterations AS incidentiterations, clients.id AS clientid, CONCAT(clients.surname, ' ', clients.name) AS clientname, clients.address AS clientaddress, clients.phone AS clientphone, clients.alternate_phone AS clientalternatephone,  objects.id as objectid, objects.latitude AS objectlatitude, objects.longitude AS objectlongitude, objects.street AS objectstreet, objects.house AS objecthouse, objects.section AS objectsection, objects.floor AS objectfloor, objects.apartment AS objectapartment, objects.category AS objectcategory, objects.pets as objectpets, timestamps.id AS timestampid, DATE_FORMAT(timestamps.date, '%d.%m.%Y') AS date, timestamps.time AS time FROM incidents, clients, timestamps, objects WHERE incidents.timestamp_id=timestamps.id and incidents.object_id=objects.id and objects.client_id=clients.id"
+  db.query(query, (error, data) => {
+    if (error) {
+      console.log(error);
+      return response.json(error);
+    }
+    return response.json(data);
+  })
+});
+
+//get opened incidents with join
 app.get("/opened-incidents", (request, response) => {
-  const query = "SELECT * FROM incidents where `is_closed` = '0'"
+  const query = "SELECT incidents.id AS incidentid, incidents.is_checked AS incidentchecked, incidents.iterations AS incidentiterations, clients.id AS clientid, CONCAT(clients.surname, ' ', clients.name) AS clientname, clients.address AS clientaddress, clients.phone AS clientphone, clients.alternate_phone AS clientalternatephone,  objects.id as objectid, objects.latitude AS objectlatitude, objects.longitude AS objectlongitude, objects.street AS objectstreet, objects.house AS objecthouse, objects.section AS objectsection, objects.floor AS objectfloor, objects.apartment AS objectapartment, objects.category AS objectcategory, objects.pets as objectpets, timestamps.id AS timestampid, DATE_FORMAT(timestamps.date, '%d.%m.%Y') AS date, timestamps.time AS time FROM incidents, clients, timestamps, objects WHERE incidents.timestamp_id=timestamps.id and incidents.object_id=objects.id and objects.client_id=clients.id and incidents.is_closed=0"
   db.query(query, (error, data) => {
     if (error) {
       console.log(error);
@@ -358,6 +370,31 @@ app.get("/unchecked-incidents", (request, response) => {
     return response.json(data);
   })
 });
+
+//set incident checked
+app.put("/check-incident/:id", (request, response) => {
+  const incidentId = request.params.id;
+  const query = "UPDATE incidents SET `is_checked` = 1 WHERE `id` = ?";
+
+  db.query(query, [incidentId], (error, data) => {
+    if (error) return response.send(error);
+    return response.json(data);
+  });
+});
+
+//get client by id
+app.get("/clients/:id", (request, response) => {
+  const clientId = request.params.id;
+  const query = "SELECT * FROM clients WHERE id = ? "
+  db.query(query, [clientId], (error, data) => {
+    if (error) {
+      console.log(error);
+      return response.json(error);
+    }
+    return response.json(data);
+  })
+});
+
 
 app.listen(8800, () => {
   console.log("Connected to backend")
